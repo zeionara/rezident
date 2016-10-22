1c8c85720ccb61fe36457c8941b4604099d47603
ZeioNara <zeionara@gmail.com>
2016-10-22 19:00:00 +0300

    Made worked copy (repositioning cursor not sound)

--- lab7.asm
+++ lab7.asm
@@ -1,32 +1,55 @@
-code segment para 'code'
+code segment para 'code'    ;определение кодового сегмента
     assume cs:code, ds:code, ss:code, es:code
+    
+    org 100h                ;размер PSP для COM программы
 
-    org 100h
-main:
-    jmp init
+
+main: jmp init              ;переход на нерезидентную часть
+
 
 ;
 ; data
 ;
 
-mes1 db ‘Program installed!$’			; сообщение при установке резидента
-mes2 db ‘Program already installed!$’	; сообщение при попытке повторной установки
-mes3 db ‘Program unload!$’			    ; сообщение при выгрузке программы
+old_09h dd 0    ;старый обработчик 09h
+mes1 db "Program installed!$"			; сообщение при установке резидента
+mes2 db "Program already installed!$"	; сообщение при попытке повторной установки
+;mes3 db "Program unload!$"			    ; сообщение при выгрузке программы
 
 ;
 ; обработчик прерывания от клавиатуры
 ;
 
 new_09h proc
+    jmp cont
+
+    mes3 db "Program unload!$"			; сообщение при выгрузке программы
+
+    cont:
+
     cmp ah, 0fah	; проверить номер функции прерывания от клавиатуры
     jne out_09h		; если не наша - обработка нажатой клавиши
     cmp al, 0		; это подфункция проверки на повторную установку?
     je inst			; если да, то сообщить о невозможности повторн.установки
-    jmp out_2fh		; неизвестная подфункция – обработка нажатой клавиши
+    jmp out_09h		; неизвестная подфункция – обработка нажатой клавиши
 
 inst:
     mov al,0ffh		; программа уже установлена
     iret			; выход из прерывания
+
+do_signal:
+    mov ah,02h
+    push bx
+    xor bh,bh
+    push dx
+    xor dx,dx
+    int 10h
+
+    pop dx
+    pop bx
+    pop ax
+    jmp cs:old_09h	; переход к старому обработчику
+
 
 out_09h:
     push ax
@@ -49,25 +72,20 @@ rezoff:
     lds dx, cs:old_09h	; ds:dx – вектор старого 09h
     int  21h
 
-    mov es, cs:2Ch		; выгрузка области окружения
-    mov ah, 49h		; функция освобождения памяти
-    int 21h
+    ;mov es, cs:2Ch		; выгрузка области окружения
+    ;mov ah, 49h		; функция освобождения памяти
+    ;int 21h
 
     push  cs
     pop  es			; выгрузка резидента 
     mov  ah,  49h
     int  21h
 
-    ;mov ah,09h		; функция вывода на экран
-    ;lea dx,mes3;	; DS:DX - адрес строки
-    ;int 21h
-    
     pop dx
     pop es
-    pop  ds
-    iret
-
-    old_09h dd 0    ;старый обработчик 09h
+    pop ds
+    pop ax
+    jmp cs:old_09h	; переход к старому обработчику
 
 new_09h endp
 
@@ -83,14 +101,14 @@ init proc
     mov al, 0		; на наличие резидентной программы в оперативной памяти
     int 09h
 
-    cmp al,0ffh		; программа установлена?
-    je if_instaled		; если да, то перейти к выводу предупрежд. сообщения
+    cmp al,0ffh		    ; программа установлена?
+    je if_installed		; если да, то перейти к выводу предупрежд. сообщения
                         ; сохранить вектор 09h
 	mov ax,3509h		; функция получения вектора 09h
 	int 21h
 
-    mov word prt cs:old_09h,bx		; сохранить смещение системного обработчика
-    mov word prt cs:old_09h+2,es	; сохранить сегмент системного обработчика
+    mov word ptr cs:old_09h,bx		; сохранить смещение системного обработчика
+    mov word ptr cs:old_09h+2,es	; сохранить сегмент системного обработчика
                                     ; заполнить вектор 09h
     mov ax,2509h			; функция установления вектора прерывания 09h
     mov dx,offset new_09h	; смещение нового обработчика
@@ -101,7 +119,8 @@ init proc
     int 21h
                         ; остаться резидентом
     mov ax,3100h		; функция «завершиться и остаться резидентом»
-    mov dx,(resident-main+10fh)/16	; размер в параграфах
+    mov dx,(resident-main+10Fh)/16	; размер в параграфах
+    int 21h
 
     mov ax,4c00h
     int 21h
@@ -111,7 +130,7 @@ if_installed:
     lea dx,mes2			; DS:DX - адрес строки
     int 21h
     
-    mov ax,4c01h		; функция завершения с кодом возврата
+    mov ax,4c00h		; функция завершения с кодом возврата
     int 21h
 
 init endp
